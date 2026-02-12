893e404fc7b231f83ea9fabd1e3503348ebe2f26
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-13 00:26:14 +0100

    init commit

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
