9ba02fcecde0f3ab785f3a8700361a08d104153c
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-14 20:24:47 +0100

    use renamed gnome and gdm service options

--- system/desktop.nix
+++ system/desktop.nix
@@ -5,8 +5,8 @@
   services.xserver.enable = true;
 
   # GNOME Desktop Environment
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Printing
   services.printing.enable = true;
