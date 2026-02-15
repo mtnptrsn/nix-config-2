a833493ebcf8397240be345ba80f47f9abb84661
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-15 03:54:38 +0100

    enable auto-login for mtnptrsn via GDM

--- system/desktop.nix
+++ system/desktop.nix
@@ -4,6 +4,8 @@
   # GNOME Desktop Environment
   services.displayManager.gdm.enable = true;
   services.displayManager.gdm.wayland = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "mtnptrsn";
   services.desktopManager.gnome.enable = true;
 
   # Printing
