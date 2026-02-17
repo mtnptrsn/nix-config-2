f8de7fe1a789cb3ca0fc47d2158182a75018cbc1
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-17 18:51:17 +0100

    add 1Password and Ctrl+Number extensions to Firefox

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -26,6 +26,14 @@ in
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
         };
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+        };
+        "{84601290-bec9-494a-b11c-1baa897a9683}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ctrl-number-to-switch-tabs/latest.xpi";
+        };
       };
     };
   };
