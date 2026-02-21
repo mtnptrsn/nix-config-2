3baac0200c76ca88eb6c9cf410d7d32d8e916f77
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-22 00:43:55 +0100

    darwin: add fonts, askpass, homebrew casks, and primaryUser

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,12 +1,37 @@
-_: {
+{ pkgs, ... }:
+let
+  askpass = pkgs.writeShellScriptBin "askpass" ''
+    osascript -e 'Tell application "System Events" to display dialog "Password for sudo:" with hidden answer default answer ""' -e 'text returned of result'
+  '';
+in
+{
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
+
+  fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
+
+  environment.systemPackages = [ askpass ];
+  environment.variables.SUDO_ASKPASS = "${askpass}/bin/askpass";
 
   # Managed by the Determinate Nix installer
   nix.enable = false;
 
+  system.primaryUser = "mtnptrsn";
+
   users.users.mtnptrsn = {
     home = "/Users/mtnptrsn";
+  };
+
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = true;
+    user = "mtnptrsn";
+  };
+
+  homebrew = {
+    enable = true;
+    casks = [ "macwhisper" ];
+    onActivation.autoUpdate = false;
   };
 
   system.stateVersion = 4;
