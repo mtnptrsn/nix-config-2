8419701f609c33c44a1e6a3eab213a4f4ce64a99
Mårten Pettersson <mtnptrsn@gmail.com>
2026-02-21 16:35:37 +0100

    firefox: add uBlock Origin and comment extension IDs

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -18,18 +18,27 @@ in
         };
       };
       policies.ExtensionSettings = {
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        # React DevTools
         "@react-devtools" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/react-devtools/latest.xpi";
         };
+        # Vimium
         "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
         };
+        # 1Password
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
         };
+        # Ctrl+Number to switch tabs
         "{84601290-bec9-494a-b11c-1baa897a9683}" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ctrl-number-to-switch-tabs/latest.xpi";
