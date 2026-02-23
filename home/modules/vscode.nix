823e72d4f7711c3eadd8031c4a0d247db03eea18
Mårten Pettersson <marten.pettersson@finch3d.com>
2026-02-24 00:33:06 +0100

    fix(vscode): migrate extensions to profile-based config

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -13,7 +13,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
+      profiles.default.extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         eamodio.gitlens
         esbenp.prettier-vscode
