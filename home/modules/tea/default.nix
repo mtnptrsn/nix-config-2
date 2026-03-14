9b4508696349c2117865eac41e2b2995fa55cc6a
Mårten Pettersson <mtnptrsn@gmail.com>
2026-03-14 10:34:51 +0100

    feat(tea): add zsh tab completion

--- home/modules/tea/default.nix
+++ home/modules/tea/default.nix
@@ -21,5 +21,29 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [ tea ];
+
+    programs.zsh.initContent = lib.mkIf config.modules.zsh.enable ''
+      _tea() {
+        local -a commands
+        commands=(add open project projects)
+
+        if (( CURRENT == 2 )); then
+          _describe 'command' commands
+        elif [[ "$words[2]" == "project" ]]; then
+          if (( CURRENT == 3 )); then
+            local pdir="${cfg.notesDir}/projects"
+            pdir="''${pdir/#\~/$HOME}"
+            if [[ -d "$pdir" ]]; then
+              local -a projects
+              projects=(''${pdir}/*.md(N:t:r))
+              _describe 'project' projects
+            fi
+          elif (( CURRENT == 4 )); then
+            _describe 'subcommand' '(add)'
+          fi
+        fi
+      }
+      compdef _tea tea
+    '';
   };
 }
