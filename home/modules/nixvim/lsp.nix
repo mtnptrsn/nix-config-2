63c86bbc0f7ade2bdb55eeb685a8408a68ce7f07
Mårten Pettersson <mtnptrsn@gmail.com>
2026-06-04 09:30:49 +0200

    feat(nixvim): use hover for gp keymap instead of peek definition

--- home/modules/nixvim/lsp.nix
+++ home/modules/nixvim/lsp.nix
@@ -38,20 +38,12 @@
           mode = "n";
           options.desc = "Document symbols";
         }
-        # Show definition in a floating preview without leaving current position
+        # Show hover info (type signature, docs) for the symbol under cursor
         {
           key = "gp";
-          action.__raw = ''
-            function()
-                    local params = vim.lsp.util.make_position_params(0)
-                    vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result)
-                      if err or not result then return end
-                      local def = vim.islist(result) and result[1] or result
-                      vim.lsp.util.preview_location(def, { border = "single" })
-                    end)
-                  end'';
+          action.__raw = "vim.lsp.buf.hover";
           mode = "n";
-          options.desc = "Peek definition";
+          options.desc = "Hover";
         }
         {
           key = "gd";
