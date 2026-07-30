b35b6939d5f8a9df70aa2fe792f540ebf08f202a
Mårten Pettersson <mtnptrsn@gmail.com>
2026-07-30 22:48:43 +0200

    feat(vscode): follow shared theme selector with catppuccin

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -6,6 +6,8 @@
 }:
 let
   cfg = config.modules.vscode;
+
+  colorTheme = if config.modules.theme == "light" then "Catppuccin Latte" else "Catppuccin Mocha";
 in
 {
   options.modules.vscode.enable = lib.mkEnableOption "vscode";
@@ -13,12 +15,16 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        eamodio.gitlens
-        esbenp.prettier-vscode
-        dbaeumer.vscode-eslint
-      ];
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+          eamodio.gitlens
+          esbenp.prettier-vscode
+          dbaeumer.vscode-eslint
+          catppuccin.catppuccin-vsc
+        ];
+        userSettings."workbench.colorTheme" = colorTheme;
+      };
     };
   };
 }
